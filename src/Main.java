f85e5b7ed9f52953f3d65c5c258b15131ca4836c
karen gevorgyan <karen-gev.22@mail.ru>
2022-09-21 19:31:07 +0300

    сделано дз2 поправление

--- src/Main.java
+++ src/Main.java
@@ -32,17 +32,23 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
-        var boxer1 = 78.2;
-        var boxer2 = 82.7;
+        var boxer1 =78.2;
+        var boxer2 =82.7;
         var totalWeight = boxer1 + boxer2;
-        System.out.println("общий вес боксеров" + totalWeight );
+        System.out.println("общий вес боксеров" + totalWeight);
         var overWeight = (boxer2 - boxer1);
         System.out.println("разница между весами бойцов составляет" + overWeight + "кг!");
         var weightDistinctionBoxer = boxer2 % boxer1;
-        System.out.println("отличие  между боксерами" + weightDistinctionBoxer + "кг!");
+        System.out.println("отличие между боксерами" + weightDistinctionBoxer + "кг!");
         var totalTime = 640;
         var employersTime = 8;
-        System.out.println("Всего работнтков в компании -  чел." + totalTime / employersTime);
+        var totalEmployees = totalTime / employersTime;
+        System.out.println("всего работников в компании - чел." + totalTime / employersTime);
+        totalEmployees = totalEmployees + 94;
+        totalTime = totalEmployees * totalTime;
+        System.out.println("Если в компании работает  " + totalEmployees +   "человек, то всего"  + totalTime +   "часов работы можно поделено между сотрудниками" );
+
+
 
 
 
